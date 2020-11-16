e0539adfbcb482d9079eb10719919a94c221868c
Marco <macroxx120@gmail.com>
2020-11-16 14:24:32 -0500

    added main class to GUI

---
+++ src/ui/Main.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[]args) {
+		launch();
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
